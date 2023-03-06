1dc526d3c77e766b4ac991d3cb80c9fddac84cfb
rudyxwhite <brandonrwing98@gmail.com>
2023-03-06 10:14:14 -0600

    got some routes working

--- develop/routes/routes.js
+++ develop/routes/routes.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const app = express();
+// This single line is equivilent to the two lines above
 const router = express.Router();
+const fs = require('fs');
+
 const path = require('path');
 
-module.exports = router
+const data = require('../db/db.json');
+
+// ALL routes in this file are prefixed with '/api'
+router.get('/notes', (req, res) => {
+    console.log("Dataset: ", data);
+    console.log("Type: ", typeof data);
+
+    // we send data back to the client
+    res.json(JSON.parse(data));
+})
+
+router.post('/notes', (req, res) => {
+    console.log(req.body);
+})
+
+
+router.delete('/notes', (req, res) => {
+
+})
+
+// this allows to be accessed outsife of this file
+module.exports = router;
--- develop/server.js
+++ develop/server.js
@@ -1,3 +1,34 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// we create an INSTANCE of our APP (SERVER)
 const app = express();
-const routes = require('./routes/routes.js')
+
+
+const apiRoutes = require('./routes/routes.js')
+const PORT = 3001;
+
+// Middlewear Setup
+// These two lines PARSE the incoming REQUEST 
+app.use(express.json());
+app.use(express.urlencoded({extended: true }))
+
+
+app.use('/api', apiRoutes);
+
+// We start defining our ROUTES
+app.get('/notes', (req, res) => {
+  //  console.log("Path: ", __dirname);
+  //console.log("Request Object: ", req)
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.get('/', (req, res) => {
+    console.log("Path: ", __dirname);
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
